e698eff3ca47f363329cd5eca9c7ac6736bcc02b
s1ddhant-tyagi <53624966+s1ddhant-tyagi@users.noreply.github.com>
2021-01-02 07:46:06 +0530

    updated icons

--- script.js
+++ script.js
@@ -22,17 +22,23 @@ class addTask
         addedTask.type = 'text';
 
         let editButton = document.createElement('button');
-        editButton.innerHTML = 'EDIT';
+        let editIcon = document.createElement('i');
         editButton.classList.add('editButton');
+        editIcon.classList.add('fa');
+        editIcon.classList.add('fa-pencil-square-o');
 
         let deleteButton = document.createElement('button');
-        deleteButton.innerHTML = 'DELETE';
+        let deleteIcon = document.createElement('i');
         deleteButton.classList.add('deleteButton');
+        deleteIcon.classList.add('fa');
+        deleteIcon.classList.add('fa-trash');
 
         container.appendChild(itemBox);
         itemBox.appendChild(addedTask);
         itemBox.appendChild(editButton);
         itemBox.appendChild(deleteButton);
+        editButton.appendChild(editIcon);
+        deleteButton.appendChild(deleteIcon);
 
         editButton.addEventListener('click', ()=>
         {
